7523e2d693de4e9854dbfa095b40b4a2df88552b
moreno <moreno.ferreira11@gmail.com>
2024-12-04 17:15:12 -0300

    .

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,16 @@
 import { configureStore, Store } from "@reduxjs/toolkit";
-
-import carrinhoReducer from './reducers/carrinho'
-import { favoritar } from "./reducers/favorito";
+import carrinhoReducer from './reducers/carrinho';
+import  favoritarReducer  from "./reducers/favorito";
 import api from "../services/api";
 
 export const store = configureStore({
   reducer: {
     carrinho: carrinhoReducer,
-    [api.reducerPath]: api.reducer
+    [api.reducerPath]: api.reducer,  // Aqui foi adicionada a vírgula
+    favoritar: favoritarReducer,  // Corrigido com a vírgula no final da linha anterior
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware)
+    getDefaultMiddleware().concat(api.middleware),  // Garantindo a concatenação correta do middleware
+});
 
-})
-
-export type RootReducer = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
