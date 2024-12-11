391efe9b99dacd9bd54c4f67daa0c959bd62b1b0
moreno <moreno.ferreira11@gmail.com>
2024-12-11 18:00:18 -0300

    .

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,42 +1,43 @@
-import { useDispatch, useSelector } from 'react-redux'  // Importando useSelector
+import { useDispatch } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
 import { useGetProdutosQuery } from '../services/api'
-import { adicionarFav } from '../store/reducers/favoritos'  // Certifique-se de que a ação 'adicionarFav' está correta
+
 import * as S from './styles'
 
-const ProdutosComponent = () => {
+const produtosComponent = () => {
   const dispatch = useDispatch()
 
-  // Usando useSelector para pegar os favoritos da store
-  const favoritos = useSelector((state: any) => state.favoritar.itens)
 
-  const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const favoritosIds = new Set(favoritos.map((f) => f.id))
-    return favoritosIds.has(produto.id)
-  }
 
-  const { data: produtos, isLoading, isError } = useGetProdutosQuery()
+  const ProdutosComponent = ({
+    favoritos,
+    favoritar
+  }: Props) => {
+    const produtoEstaNosFavoritos = (produto: ProdutoType) => {
+      const produtoId = produto.id
+      const IdsDosFavoritos = favoritos.map((f) => f.id)
 
-  if (isLoading) return <h2>Carregando...</h2>
-  if (isError) return <h2>Erro ao carregar os produtos</h2> // Tratamento de erro
+      return IdsDosFavoritos.includes(produtoId)
+    }
+    const {data: produtos, isLoading} = useGetProdutosQuery()
 
-  const favoritar = (produto: ProdutoType) => {
-    dispatch(adicionarFav(produto)) // Usando o dispatch para adicionar aos favoritos
-  }
-
-  return (
-    <S.Produtos>
-      {produtos?.map((produto) => (
-        <Produto
+    if(isLoading) return <h2>Carregando...</h2>
+    return (
+      <>
+      <S.Produtos>
+        {produtos?.map((produto) => (
+          <Produto
           estaNosFavoritos={produtoEstaNosFavoritos(produto)}
           key={produto.id}
           produto={produto}
           favoritar={favoritar}
-        />
-      ))}
-    </S.Produtos>
+          />
+        ))}
+      </S.Produtos>
+    </>
   )
+}
 }
 
 export default ProdutosComponent
